18b76d55db31ccd166030310990ff713ce559e24
Davit <davosim@mail.ru>
2021-11-04 15:06:11 +0400

    DynamicArray

---
+++ src/homework/dynamicArray/DynamicArray.java
@@ -0,0 +1,42 @@
+package homework.dynamicArray;
+
+public class DynamicArray {
+  private   int[] array = new int[10];
+   private int size = 0;
+
+   public void add(int valeum) {
+        array[size] = valeum;
+        size++;
+        if (size == array.length) {
+            extend();
+        }
+
+
+    }
+
+   private void extend() {
+        int[] newArray = new int[array.length + 10];
+        for (int i = 0; i < array.length; i++) {
+            newArray[i] = array[i];
+
+        }
+        array = newArray;
+    }
+
+
+  public   int getByIndex(int index) {
+        if (index > array.length) {
+            System.out.println();
+
+        } else return array[index];
+
+        return -1;
+    }
+
+  public   void print() {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != 0)
+                System.out.print(array[i] + " ");
+        }
+    }
+}
