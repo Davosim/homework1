6bb87adf089bb9440e25b91d410ec901c4b6c1b2
Davit <davosim@mail.ru>
2021-11-10 02:33:59 +0400

    extend

--- src/homework/Bracechecker/Stack.java
+++ src/homework/Bracechecker/Stack.java
@@ -1,9 +1,19 @@
 package homework.Bracechecker;
 
+import homework.dynamicArray.DynamicArray;
+
 public class Stack {
 
-    private char[] array = new char[50];
+    private char[] array = new char[10];
     private int size;
+    DynamicArray dynamicArray = new DynamicArray();
+
+    private void add(int valeum) {
+        if (size == array.length) {
+            dynamicArray.extend();
+        }
+        array[size++] = (char) valeum;
+    }
 
     public Stack() {
         size = -1;
